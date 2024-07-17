4def0280ff23fbd72bc5bd83389674387324342e
tsega7664 <yeabsira.zelalem@aastustudent.edu.et>
2024-07-17 14:08:06 +0300

    modified

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Footer = () => {
   return (
     <div className='bg-customTeal  w-full text-center p-4 text-white font-bold'>
-        <p className='p-2 text-sm'>Created by <span className='text-customBlue1 '>Yeabsira</span> &copy; All right reserved</p>
+        <p className='p-2 text-sm'>Created by <span className='text-customBlue1 '>Yeabsira Zelalem</span> &copy; All right reserved</p>
         <p>@2024</p>
     </div>
   )
